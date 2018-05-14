4daf1cca4984edb734f39d27f88c98e1f901ffdb
Unknown <ruanlx22@gmail.com>
2018-05-14 17:22:28 +0200

    add marks on map

---
+++ company.php
@@ -0,0 +1,153 @@
+<?php
+
+class company
+{
+ private $id;
+ private $name;
+ private $lat;
+ private $lon;
+ private $description;
+ private $url;
+ private $img;
+ private $category;
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $lat
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param mixed $lon
+     */
+    public function setLon($lon)
+    {
+        $this->lon = $lon;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLon()
+    {
+        return $this->lon;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @param mixed $img
+     */
+    public function setImg($img)
+    {
+        $this->img = $img;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * @param mixed $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function __construct($id,$name,$lat,$lon,$description,$url,$img,$category)
+    {
+        $this->id=$id;
+        $this->name=$name;
+        $this->lat=$lat;
+        $this->lon=$lon;
+        $this->description=$description;
+        $this->url=$url;
+        $this->img=$img;
+        $this->category=$category;
+    }
+}
--- map.php
+++ map.php
@@ -1,3 +1,38 @@
+<?php
+require_once('company.php');
+$USER = 'root';
+$PASSWORD = '';
+$SERVER = 'localhost';
+$DB = 'GOLNG';
+
+$conn = mysqli_connect($SERVER, $USER, $PASSWORD, $DB);
+
+if (!$conn){
+    die("Connection to DB failed :" . mysqli_connect_error() . "</br>");
+}
+
+echo  "Successfully connected to DB!" . "</br>";
+
+$sql = "SELECT * FROM company";
+$results = mysqli_query($conn,$sql);
+if ($results){
+    echo "company info get successfully!" . "</br>";
+} else {
+    echo "Error get company info!" . $connection->error . "</br>" ;
+}
+$count = 0;
+$companyArray = [];
+while($row = $results->fetch_assoc()){
+//    $count++;
+    $company = new company($row['id'],$row['name'],$row['lat'],$row['lon'],$row['description'],$row['url'],$row['img'],$row['category']);
+    $companyArray[] = $company;
+}
+
+//foreach ($companyArray as $sc){
+//    echo $sc->getLat()."  ".$sc->getLon()."<br>";
+//}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +40,6 @@
         <style>
             #map {
                 height: 100%;
-                width: 100%;
             }
             html, body {
                 height: 100%;
@@ -16,11 +50,194 @@
     </head>
     <body>
     <div id="map"></div>
-    <div><p>aaa</p></div>
-    <script type="text/javascript" src="style.js"> 
-    </script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtelxbrcYzsm362x1oz4vOZaLLQp6lv0k&callback=initMap"
             async defer>        
     </script>
+    <script type="text/javascript" >
+        function initMap() {
+            // var myLabel2 = {lat: 59.304,lng: 18.080};
+            // var myLabel = {lat:-25.363, lng:131.044};
+            var map = new google.maps.Map(document.getElementById('map'),{
+                zoom: 5,
+                center: {lat: 59.304,lng: 18.080},
+                styles: [
+                    {
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "color": "#f5f5f5"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "administrative.land_parcel",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "administrative.neighborhood",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.man_made",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.natural",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.natural.landcover",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape.natural.terrain",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "labels.text",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.business",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "elementType": "geometry",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi.park",
+                        "elementType": "labels.icon",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "labels",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "labels.icon",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "labels.text",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    }
+                ]
+            });
+            // var marker = new google.maps.Marker({
+            //     position: myLabel,
+            //     map: map
+            // });
+            // var marker2 = new google.maps.Marker({
+            //     position: myLabel2,
+            //     map: map
+            // });
+            <?php
+            foreach ($companyArray as $sc){
+                $count++;
+                if($sc->getLat()!=null&&$sc->getLon()!=null){
+                    echo "
+                    var marker".$count." = new google.maps.Marker({
+                    position: {lat:".$sc->getLat()." ,lng:".$sc->getLon()."},
+                    map:map
+                    });
+                    ";
+                }
+
+            }
+            ?>
+        }
+    </script>
+
     </body>
 </html>
